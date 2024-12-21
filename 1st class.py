8fd3f496d401eff78b49c84838c7ad5bec266c27
AbdullahAlJawad <abdullahaljawad95@gmail.com>
2024-12-21 22:16:22 +0600

    Add files via upload

---
+++ 1st class.py
@@ -0,0 +1,30 @@
+#single line comment
+'''
+multiple line comment
+'''
+print("step1: start")
+print("step2: declare x,y and sum")
+print("step3: assign value of every variable")
+print("step4: display sum")
+print("step5: stop")
+
+name="Glenn Maxwell"
+print("Glenn Maxwell")
+print(name)
+print("My favourite player is ",name)
+profession="cricketer"
+print(f"My name is {name}. My profession is {profession}")
+
+student_name="Jawad"
+grade=9
+city="Dhaka"
+print(f"My name is {student_name}. I am in grade {grade}. I live in {city}.")
+#python characteristic
+
+#python case sensitive
+
+name="rafa"
+
+Name="rafa"
+
+#python use indentation to run itself set of block codes. space or tab use to active indentation
